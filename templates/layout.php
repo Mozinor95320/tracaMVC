df35eb58039e5886343751b5c9a8830799a5b301
Mozinor95320 <43842362+Mozinor95320@users.noreply.github.com>
2024-09-30 18:45:35 +0200

    Update layout.php

--- templates/layout.php
+++ templates/layout.php
@@ -8,12 +8,12 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"/>
         <!-- Inclusion de Bootstrap Icons -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css"/>
-        
+        <link rel="stylesheet" href="style.css"/>
 
     </head>
 
     <body>
-        <header class="container-fluid">
+        <header>
             <div class="row align-items-center">
                 <div class="col-md-3">
                     <h1>Parker</h1>
