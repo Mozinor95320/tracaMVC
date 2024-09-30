fce68022c3906a66b4058ec2a81171f186c16fe0
Mozinor95320 <43842362+Mozinor95320@users.noreply.github.com>
2024-09-30 13:51:33 +0200

    Update homepage.php

--- src/controllers/homepage.php
+++ src/controllers/homepage.php
@@ -3,7 +3,7 @@
 namespace Application\Controllers\Homepage;
 
 require_once('src/lib/database.php');
-require_once('src/model/post.php');
+require_once('src/model/TracabilitySheet.php');
 
 use Application\Lib\Database\DatabaseConnection;
 use Application\Model\TracabilitySheet\TracabilitySheetRepository;
@@ -12,9 +12,9 @@ class Homepage
 {
     public function execute()
     {
-        $postRepository = new PostRepository();
-        $postRepository->connection = new DatabaseConnection();
-        $posts = $postRepository->getPosts();
+        $tracabilitySheetRepository = new TracabilitySheetRepository();
+        $tracabilitySheetRepository->connection = new DatabaseConnection();
+        $tracabilitySheets = $tracabilitySheetRepository->getTracabilitySheets();
 
         require('templates/homepage.php');
     }
