10f5920ca42a95c4b026cf93e90878c9d5493a13
Mozinor95320 <43842362+Mozinor95320@users.noreply.github.com>
2024-09-30 15:58:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 
-require_once('src/controllers/comment/add.php');
-require_once('src/controllers/comment/update.php');
 require_once('src/controllers/homepage.php');
-require_once('src/controllers/tracabilitySheet.php');
 
 use Application\Controllers\Comment\Add\AddComment;
 use Application\Controllers\Comment\Update\UpdateComment;
@@ -12,18 +9,6 @@ use Application\Controllers\TracabilitySheet\TracabilitySheet;
 
 try {
     if (isset($_GET['action']) && $_GET['action'] !== '') {
-        if ($_GET['action'] === 'post') {
-            if (isset($_GET['id']) && $_GET['id'] > 0) {
-                $identifier = $_GET['id'];
-
-                (new TracabilitySheet())->execute($identifier);
-            } else {
-                throw new Exception('Aucun identifiant de billet envoyé');
-            }
-        }  
-        else {
-            throw new Exception("La page que vous recherchez n'existe pas.");
-        }
     } else {
         (new Homepage())->execute();
     }
