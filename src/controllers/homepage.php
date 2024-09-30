eee73fc5098222301d534fa0ff135195eec40ba6
Mozinor95320 <43842362+Mozinor95320@users.noreply.github.com>
2024-09-30 17:18:06 +0200

    Update homepage.php

--- src/controllers/homepage.php
+++ src/controllers/homepage.php
@@ -3,6 +3,7 @@
 namespace Application\Controllers\Homepage;
 
 require_once('src/lib/database.php');
+require_once('src/model/tracabilitySheet.php');
 
 use Application\Lib\Database\DatabaseConnection;
 use Application\Model\TracabilitySheet\TracabilitySheetRepository;
