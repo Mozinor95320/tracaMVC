ece1536ed8c92dc9a5d5609a763103f476fd23e5
Mozinor95320 <43842362+Mozinor95320@users.noreply.github.com>
2024-09-30 18:13:02 +0200

    Update layout.php

--- templates/layout.php
+++ templates/layout.php
@@ -8,7 +8,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
         <!-- Inclusion de Bootstrap Icons -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
-        <link href="style.css" rel="stylesheet" />
+        <link href="style.css" rel="stylesheet">
 
     </head>
 
