454a1c2950ec6b2541603cf712018356b9e8a65a
Mozinor95320 <43842362+Mozinor95320@users.noreply.github.com>
2024-09-30 17:25:21 +0200

    Update layout.php

--- templates/layout.php
+++ templates/layout.php
@@ -1,30 +1,12 @@
 <!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<html>
+    <head>
+        <meta charset="utf-8" />
         <title><?= $title ?></title>
-        <!-- Inclusion de Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-        <!-- Inclusion de Bootstrap Icons -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
-        <link rel="stylesheet" href="style.css">
-</head>
+        <link href="style.css" rel="stylesheet" />
+    </head>
 
-<body>
-<header class="container-fluid">
-    <div class="row align-items-center">
-        <div class="col-md-3">
-            <h1>Parker</h1>
-        </div>
-    </div>
-    <div class="row align-items-center">
-    </div>
-</header>
-    
-    <?= $content ?>
-<!-- Inclusion de Bootstrap JS et de son bundle -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-
+    <body>
+        <?= $content ?>
+    </body>
 </html>
