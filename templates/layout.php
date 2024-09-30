c8ed7eeb2027af5c73ed4d50fe52347536aeb529
Mozinor95320 <43842362+Mozinor95320@users.noreply.github.com>
2024-09-30 17:36:58 +0200

    Update layout.php

--- templates/layout.php
+++ templates/layout.php
@@ -2,8 +2,14 @@
 <html>
     <head>
         <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title><?= $title ?></title>
+        <!-- Inclusion de Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Inclusion de Bootstrap Icons -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
         <link href="style.css" rel="stylesheet" />
+
     </head>
 
     <body>
