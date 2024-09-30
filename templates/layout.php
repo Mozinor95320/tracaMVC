f21c5687dd031ccfa274052afc17d89cbc3198e0
Mozinor95320 <43842362+Mozinor95320@users.noreply.github.com>
2024-09-30 09:42:09 +0200

    Update layout.php

--- templates/layout.php
+++ templates/layout.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?= $title ?></title>
-        <link href="style.css" rel="stylesheet" />
-    </head>
+        <!-- Inclusion de Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Inclusion de Bootstrap Icons -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+        <link rel="stylesheet" href="style.css">
+</head>
 
     <body>
         <?= $content ?>
-    </body>
+
+    <!-- Inclusion de Bootstrap JS et de son bundle -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
 </html>
