eec8a3e9fbf254f08f1c09f9718c0e37eadda66e
Mozinor95320 <43842362+Mozinor95320@users.noreply.github.com>
2024-09-30 17:58:02 +0200

    Update homepage.php

--- templates/homepage.php
+++ templates/homepage.php
@@ -1,19 +1,34 @@
 <?php $title = "Le blog de l'AVBN"; ?>
 
 <?php ob_start(); ?>
-<h1>Le super blog de l'AVBN !</h1>
-<p>Derniers billets du blog :</p>
 
 <?php
 foreach ($tracabilitySheets as $tracabilitySheet) {
 ?>
-    <div class="news">
-        <h3>
-            <?= htmlspecialchars($tracabilitySheet->identifier); ?>
-            <em>le <?= $tracabilitySheet->frenchCreationDate; ?></em>
-        </h3>
-            <em><a href="index.php?action=post&id=<?= urlencode($tracabilitySheet->identifier) ?>">Commentaires</a></em>
+<div class="container mt-5">
+    <h1 class="text-center mb-4">Recherche par SN</h1>
+    <!-- Barre de recherche -->
+    <form id="search-form" method="GET" action="" class="mb-4">
+        <div class="input-group">
+            <input type="text" id="search-input" name="search-sn" class="form-control" placeholder="Rechercher un SN" required>
+            <button class="btn btn-primary" type="submit">Rechercher</button>
+        </div>
+    </form>
+    <div id="result-container">
+        <div class="card mb-3">
+            <div class="card-body d-flex justify-content-between align-items-center">';
+                <div>
+                    <h5 class="card-title fw-bold"><?= htmlspecialchars($tracabilitySheet->identifier); ?></h5>
+                    <em class="card-text text-muted">le :<?= $tracabilitySheet->frenchCreationDate; ?></em>
+                </div>
+                <a class="btn btn-outline-primary" href="index.php?action=post&id=<?= urlencode($tracabilitySheet->identifier) ?>">
+                    <i class="bi bi-folder2-open" style="font-size: 24px;"></i> <!--Icône de dossier ouvert-->
+                </a>
+            </div>
+        </div>
     </div>
+</div>
+        
 <?php
 }
 ?>
