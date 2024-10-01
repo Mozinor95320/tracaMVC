8ed792cf9f1df41c7dcd9c98ef8ef7ed2df42595
Mozinor95320 <43842362+Mozinor95320@users.noreply.github.com>
2024-10-01 09:31:37 +0200

    Update updateTracabilitySheet.php

--- templates/updateTracabilitySheet.php
+++ templates/updateTracabilitySheet.php
@@ -1,36 +1,7 @@
-<?php $title = "Liste des fiches de tracabilité"; ?>
+<?php $title = "Fiche n°".$tracabilitySheet->identifier; ?>
 
 <?php ob_start(); ?>
-<div class="container mt-5">
-    <h1 class="text-center mb-4">Recherche par SN</h1>
-    <!-- Barre de recherche -->
-    <form id="search-form" method="GET" action="" class="mb-4">
-        <div class="input-group">
-            <input type="text" id="search-input" name="search-sn" class="form-control" placeholder="Rechercher un SN" required>
-            <button class="btn btn-primary" type="submit">Rechercher</button>
-        </div>
-    </form>
-    <div id="result-container">
-<?php
-foreach ($tracabilitySheets as $tracabilitySheet) {
-?>
 
-        <div class="card mb-3">
-            <div class="card-body d-flex justify-content-between align-items-center">
-                <div>
-                    <h5 class="card-title fw-bold"><?= htmlspecialchars($tracabilitySheet->identifier); ?></h5>
-                    <em class="card-text text-muted">date de création de la fiche : <?= $tracabilitySheet->frenchCreationDate; ?></em>
-                </div>
-                <a class="btn btn-outline-primary" href="index.php?action=openTracabiltySheet&id=<?= urlencode($tracabilitySheet->identifier) ?>">
-                    <i class="bi bi-folder2-open" style="font-size: 24px;"></i> <!--Icône de dossier ouvert-->
-                </a>
-            </div>
-        </div>     
-<?php
-}
-?>
-    </div>
-</div>
 <?php $content = ob_get_clean(); ?>
 
 <?php require('layout.php') ?>
