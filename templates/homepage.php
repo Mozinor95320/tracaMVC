79a470157741475100be6c14c77775f789f48f1e
Mozinor95320 <43842362+Mozinor95320@users.noreply.github.com>
2024-10-01 17:30:15 +0200

    ggg

--- templates/homepage.php
+++ templates/homepage.php
@@ -15,17 +15,17 @@
 foreach ($tracabilitySheets as $tracabilitySheet) {
 ?>
 
-        <div class="card mb-3">
-            <div class="card-body d-flex justify-content-between align-items-center">
-                <div>
-                    <h5 class="card-title fw-bold"><?= htmlspecialchars($tracabilitySheet->identifier); ?></h5>
-                    <em class="card-text text-muted">date de création de la fiche : <?= $tracabilitySheet->frenchCreationDate; ?></em>
-                </div>
-                <a class="btn btn-outline-primary" href="index.php?action=openTracabilitySheet&id=<?= urlencode($tracabilitySheet->identifier) ?>">
-                    <i class="bi bi-folder2-open" style="font-size: 24px;"></i> <!--Icône de dossier ouvert-->
-                </a>
+    <div class="card mb-3">
+        <div class="card-body d-flex justify-content-between align-items-center">
+            <div>
+                <h5 class="card-title fw-bold"><?= htmlspecialchars($tracabilitySheet->identifier); ?></h5>
+                <em class="card-text text-muted">date de création de la fiche : <?= $tracabilitySheet->frenchCreationDate; ?></em>
             </div>
-        </div>     
+            <a class="btn btn-outline-primary" href="index.php?action=openTracabilitySheet&id=<?= urlencode($tracabilitySheet->identifier) ?>">
+                <i class="bi bi-folder2-open" style="font-size: 24px;"></i> <!--Icône de dossier ouvert-->
+            </a>
+        </div>
+    </div>     
 <?php
 }
 ?>
