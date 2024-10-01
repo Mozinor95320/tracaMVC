3f4ead7317a4be27791be7182560a1bb7aaa29ed
Mozinor95320 <43842362+Mozinor95320@users.noreply.github.com>
2024-10-01 08:48:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,38 @@ use Application\Controllers\TracabilitySheet\TracabilitySheet;
 
 try {
     if (isset($_GET['action']) && $_GET['action'] !== '') {
+        if ($_GET['action'] === 'post') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                $identifier = $_GET['id'];
+
+                (new Post())->execute($identifier);
+            } else {
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
+        } elseif ($_GET['action'] === 'addComment') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                $identifier = $_GET['id'];
+
+                (new AddComment())->execute($identifier, $_POST);
+            } else {
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
+        } elseif ($_GET['action'] === 'updateComment') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                $identifier = $_GET['id'];
+                // It sets the input only when the HTTP method is POST (ie. the form is submitted).
+                $input = null;
+                if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                    $input = $_POST;
+                }
+
+                (new UpdateComment())->execute($identifier, $input);
+            } else {
+                throw new Exception('Aucun identifiant de commentaire envoyé');
+            }
+        } else {
+            throw new Exception("La page que vous recherchez n'existe pas.");
+        }
     } else {
         (new Homepage())->execute();
     }
