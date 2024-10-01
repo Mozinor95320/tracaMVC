6dbc39f534d1977a1177d0959fc27aef64e56089
Mozinor95320 <43842362+Mozinor95320@users.noreply.github.com>
2024-10-01 14:16:57 +0200

    Update tolerance.js

--- templates/scriptTemplate/tolerance.js
+++ templates/scriptTemplate/tolerance.js
@@ -1,288 +1,292 @@
-function verifierTolerance({
+function verifyTolerance({
+
     inputId,
+
     toleranceId,
+
     tolMin = 0,
+
     tolMax = Infinity
-  }) {
+
+}) {
+
     document.getElementById(inputId).addEventListener("input", function () {
-      const inputValue = parseFloat(this.value);
-      const toleranceElement = document.getElementById(toleranceId);
-  
-      // Vérifier si aucune valeur n'est entrée
-      if (isNaN(inputValue)) {
-        toleranceElement.classList.remove(
-          "bg-tolerance-ok",
-          "bg-tolerance-wrong"
-        );
-        toleranceElement.classList.add("bg-tolerance-default");
-      }
-      // Vérifier si la valeur entrée est dans la tolérance
-      else if (inputValue >= tolMin && inputValue <= tolMax) {
-        toleranceElement.classList.remove(
-          "bg-tolerance-wrong",
-          "bg-tolerance-default"
-        );
-        toleranceElement.classList.add("bg-tolerance-ok");
-      } else {
-        toleranceElement.classList.remove(
-          "bg-tolerance-ok",
-          "bg-tolerance-default"
-        );
-        toleranceElement.classList.add("bg-tolerance-wrong");
-      }
+
+        const inputValue = parseFloat(this.value);
+
+        const toleranceElement = document.getElementById(toleranceId);
+
+        // Check if no value is entered
+        if (isNaN(inputValue)) {
+
+            toleranceElement.classList.remove(
+                "bg-tolerance-ok",
+                "bg-tolerance-wrong"
+            );
+
+            toleranceElement.classList.add("bg-tolerance-default");
+
+        }
+
+        // Check if the entered value is within tolerance
+        else if (inputValue >= tolMin && inputValue <= tolMax) {
+
+            toleranceElement.classList.remove(
+                "bg-tolerance-wrong",
+                "bg-tolerance-default"
+            );
+
+            toleranceElement.classList.add("bg-tolerance-ok");
+
+        } else {
+
+            toleranceElement.classList.remove(
+                "bg-tolerance-ok",
+                "bg-tolerance-default"
+            );
+
+            toleranceElement.classList.add("bg-tolerance-wrong");
+
+        }
+
     });
-  }
-  
-  // Appels pour "Avant"
-  function calculateMoyenneEcartTypeAvant() {
-    calculateMoyenneEcartType("Avant");
-  }
-  function calculateMasseLineiqueAvant() {
-    calculateMasseLineique("Avant");
-  }
-  
-  // Appels pour "Après"
-  function calculateMoyenneEcartTypeApres() {
-    calculateMoyenneEcartType("Apres");
-  }
-  function calculateMasseLineiqueApres() {
-    calculateMasseLineique("Apres");
-  }
-  
-  // Changement dynamique de la tolérance - Longueur L
-  verifierTolerance({
-    inputId: "longueur",
-    toleranceId: "toleranceLongueur",
+
+}
+
+// Dynamic tolerance change - Length L
+verifyTolerance({
+    inputId: "length",
+    toleranceId: "toleranceLength",
     tolMin: 413.5 - 6,
     tolMax: 413.5
-  });
-  
-  // Changement dynamique de la tolérance - Diamètre D
-  verifierTolerance({
-    inputId: "diametre",
-    toleranceId: "toleranceDiametre",
+});
+
+// Dynamic tolerance change - Diameter D
+verifyTolerance({
+    inputId: "diameter",
+    toleranceId: "toleranceDiameter",
     tolMin: 163,
     tolMax: 163 + 0.9
-  });
-  
-  // Changement dynamique de la tolérance - Masse M
-  verifierTolerance({
-    inputId: "masseM",
-    toleranceId: "toleranceMasseM",
+});
+
+// Dynamic tolerance change - Mass M
+verifyTolerance({
+    inputId: "massM",
+    toleranceId: "toleranceMassM",
     tolMax: 7650
-  });
-  
-  // Changement dynamique de la tolérance - Epaisseur Meche 1 Avant Frettage
-  verifierTolerance({
-    inputId: "epMeche1Avant",
-    toleranceId: "toleranceEpMeche1Avant",
+});
+
+// Dynamic tolerance change - Thickness Sample 1 Before Shrink Test
+verifyTolerance({
+    inputId: "thicknessSample1Before",
+    toleranceId: "toleranceThicknessSample1Before",
     tolMin: 0.23,
     tolMax: 0.3
-  });
-  
-  // Changement dynamique de la tolérance - Force Meche 1 Avant Frettage
-  verifierTolerance({
-    inputId: "FMeche1Avant",
-    toleranceId: "toleranceFMeche1Avant",
+});
+
+// Dynamic tolerance change - Force Sample 1 Before Shrink Test
+verifyTolerance({
+    inputId: "forceSample1Before",
+    toleranceId: "toleranceForceSample1Before",
     tolMin: 1820
-  });
-  
-  // Changement dynamique de la tolérance - Epaisseur Meche 2 Avant Frettage
-  verifierTolerance({
-    inputId: "epMeche2Avant",
-    toleranceId: "toleranceEpMeche2Avant",
+});
+
+// Dynamic tolerance change - Thickness Sample 2 Before Shrink Test
+verifyTolerance({
+    inputId: "thicknessSample2Before",
+    toleranceId: "toleranceThicknessSample2Before",
     tolMin: 0.23,
     tolMax: 0.3
-  });
-  
-  // Changement dynamique de la tolérance - Force Meche 2 Avant Frettage
-  verifierTolerance({
-    inputId: "FMeche2Avant",
-    toleranceId: "toleranceFMeche2Avant",
+});
+
+// Dynamic tolerance change - Force Sample 2 Before Shrink Test
+verifyTolerance({
+    inputId: "forceSample2Before",
+    toleranceId: "toleranceForceSample2Before",
     tolMin: 1820
-  });
-  
-  // Changement dynamique de la tolérance - Epaisseur Meche 3 Avant Frettage
-  verifierTolerance({
-    inputId: "epMeche3Avant",
-    toleranceId: "toleranceEpMeche3Avant",
+});
+
+// Dynamic tolerance change - Thickness Sample 3 Before Shrink Test
+verifyTolerance({
+    inputId: "thicknessSample3Before",
+    toleranceId: "toleranceThicknessSample3Before",
     tolMin: 0.23,
     tolMax: 0.3
-  });
-  
-  // Changement dynamique de la tolérance - Force Meche 3 Avant Frettage
-  verifierTolerance({
-    inputId: "FMeche3Avant",
-    toleranceId: "toleranceFMeche3Avant",
+});
+
+// Dynamic tolerance change - Force Sample 3 Before Shrink Test
+verifyTolerance({
+    inputId: "forceSample3Before",
+    toleranceId: "toleranceForceSample3Before",
     tolMin: 1820
-  });
-  
-  // Changement dynamique de la tolérance - Epaisseur Meche 4 Avant Frettage
-  verifierTolerance({
-    inputId: "epMeche4Avant",
-    toleranceId: "toleranceEpMeche4Avant",
+});
+
+// Dynamic tolerance change - Thickness Sample 4 Before Shrink Test
+verifyTolerance({
+    inputId: "thicknessSample4Before",
+    toleranceId: "toleranceThicknessSample4Before",
     tolMin: 0.23,
     tolMax: 0.3
-  });
-  
-  // Changement dynamique de la tolérance - Force Meche 4 Avant Frettage
-  verifierTolerance({
-    inputId: "FMeche4Avant",
-    toleranceId: "toleranceFMeche4Avant",
+});
+
+// Dynamic tolerance change - Force Sample 4 Before Shrink Test
+verifyTolerance({
+    inputId: "forceSample4Before",
+    toleranceId: "toleranceForceSample4Before",
     tolMin: 1820
-  });
-  
-  // Changement dynamique de la tolérance - Epaisseur Meche 5 Avant Frettage
-  verifierTolerance({
-    inputId: "epMeche5Avant",
-    toleranceId: "toleranceEpMeche5Avant",
+});
+
+// Dynamic tolerance change - Thickness Sample 5 Before Shrink Test
+verifyTolerance({
+    inputId: "thicknessSample5Before",
+    toleranceId: "toleranceThicknessSample5Before",
     tolMin: 0.23,
     tolMax: 0.3
-  });
-  
-  // Changement dynamique de la tolérance - Force Meche 5 Avant Frettage
-  verifierTolerance({
-    inputId: "FMeche5Avant",
-    toleranceId: "toleranceFMeche5Avant",
+});
+
+// Dynamic tolerance change - Force Sample 5 Before Shrink Test
+verifyTolerance({
+    inputId: "forceSample5Before",
+    toleranceId: "toleranceForceSample5Before",
     tolMin: 1820
-  });
-  
-  // Changement dynamique de la tolérance - Epaisseur Meche 1 Apres Frettage
-  verifierTolerance({
-    inputId: "epMeche1Apres",
-    toleranceId: "toleranceEpMeche1Apres",
+});
+
+// Dynamic tolerance change - Thickness Sample 1 After Shrink Test
+verifyTolerance({
+    inputId: "thicknessSample1After",
+    toleranceId: "toleranceThicknessSample1After",
     tolMin: 0.23,
     tolMax: 0.3
-  });
-  
-  // Changement dynamique de la tolérance - Force Meche 1 Apres Frettage
-  verifierTolerance({
-    inputId: "FMeche1Apres",
-    toleranceId: "toleranceFMeche1Apres",
+});
+
+// Dynamic tolerance change - Force Sample 1 After Shrink Test
+verifyTolerance({
+    inputId: "forceSample1After",
+    toleranceId: "toleranceForceSample1After",
     tolMin: 1820
-  });
-  
-  // Changement dynamique de la tolérance - Epaisseur Meche 2 Apres Frettage
-  verifierTolerance({
-    inputId: "epMeche2Apres",
-    toleranceId: "toleranceEpMeche2Apres",
+});
+
+// Dynamic tolerance change - Thickness Sample 2 After Shrink Test
+verifyTolerance({
+    inputId: "thicknessSample2After",
+    toleranceId: "toleranceThicknessSample2After",
     tolMin: 0.23,
     tolMax: 0.3
-  });
-  
-  // Changement dynamique de la tolérance - Force Meche 2 Apres Frettage
-  verifierTolerance({
-    inputId: "FMeche2Apres",
-    toleranceId: "toleranceFMeche2Apres",
+});
+
+// Dynamic tolerance change - Force Sample 2 After Shrink Test
+verifyTolerance({
+    inputId: "forceSample2After",
+    toleranceId: "toleranceForceSample2After",
     tolMin: 1820
-  });
-  
-  // Changement dynamique de la tolérance - Epaisseur Meche 3 Apres Frettage
-  verifierTolerance({
-    inputId: "epMeche3Apres",
-    toleranceId: "toleranceEpMeche3Apres",
+});
+
+// Dynamic tolerance change - Thickness Sample 3 After Shrink Test
+verifyTolerance({
+    inputId: "thicknessSample3After",
+    toleranceId: "toleranceThicknessSample3After",
     tolMin: 0.23,
     tolMax: 0.3
-  });
-  
-  // Changement dynamique de la tolérance - Force Meche 3 Apres Frettage
-  verifierTolerance({
-    inputId: "FMeche3Apres",
-    toleranceId: "toleranceFMeche3Apres",
+});
+
+// Dynamic tolerance change - Force Sample 3 After Shrink Test
+verifyTolerance({
+    inputId: "forceSample3After",
+    toleranceId: "toleranceForceSample3After",
     tolMin: 1820
-  });
-  
-  // Changement dynamique de la tolérance - Epaisseur Meche 4 Apres Frettage
-  verifierTolerance({
-    inputId: "epMeche4Apres",
-    toleranceId: "toleranceEpMeche4Apres",
+});
+
+// Dynamic tolerance change - Thickness Sample 4 After Shrink Test
+verifyTolerance({
+    inputId: "thicknessSample4After",
+    toleranceId: "toleranceThicknessSample4After",
     tolMin: 0.23,
     tolMax: 0.3
-  });
-  
-  // Changement dynamique de la tolérance - Force Meche 4 Apres Frettage
-  verifierTolerance({
-    inputId: "FMeche4Apres",
-    toleranceId: "toleranceFMeche4Apres",
+});
+
+// Dynamic tolerance change - Force Sample 4 After Shrink Test
+verifyTolerance({
+    inputId: "forceSample4After",
+    toleranceId: "toleranceForceSample4After",
     tolMin: 1820
-  });
-  
-  // Changement dynamique de la tolérance - Epaisseur Meche 5 Apres Frettage
-  verifierTolerance({
-    inputId: "epMeche5Apres",
-    toleranceId: "toleranceEpMeche5Apres",
+});
+
+// Dynamic tolerance change - Thickness Sample 5 After Shrink Test
+verifyTolerance({
+    inputId: "thicknessSample5After",
+    toleranceId: "toleranceThicknessSample5After",
     tolMin: 0.23,
     tolMax: 0.3
-  });
-  
-  // Changement dynamique de la tolérance - Force Meche 5 Apres Frettage
-  verifierTolerance({
-    inputId: "FMeche5Apres",
-    toleranceId: "toleranceFMeche5Apres",
+});
+
+// Dynamic tolerance change - Force Sample 5 After Shrink Test
+verifyTolerance({
+    inputId: "forceSample5After",
+    toleranceId: "toleranceForceSample5After",
     tolMin: 1820
-  });
-  
-  // Changement dynamique de la tolérance - df1
-  verifierTolerance({
+});
+
+// Dynamic tolerance change - df1
+verifyTolerance({
     inputId: "df1",
     toleranceId: "toleranceDF1",
     tolMin: 174,
     tolMax: 174 + 2
-  });
-  
-  // Changement dynamique de la tolérance - df2
-  verifierTolerance({
+});
+
+// Dynamic tolerance change - df2
+verifyTolerance({
     inputId: "df2",
     toleranceId: "toleranceDF2",
     tolMin: 174,
     tolMax: 174 + 2
-  });
-  
-  // Changement dynamique de la tolérance - df3
-  verifierTolerance({
+});
+
+// Dynamic tolerance change - df3
+verifyTolerance({
     inputId: "df3",
     toleranceId: "toleranceDF3",
     tolMin: 174,
     tolMax: 174 + 2
-  });
-  
-  // Changement dynamique de la tolérance - mt
-  verifierTolerance({
+});
+
+// Dynamic tolerance change - mt
+verifyTolerance({
     inputId: "mt",
     toleranceId: "toleranceMT",
     tolMax: 8700
-  });
-  
-  document.getElementById("mt").addEventListener("input", function () {
+});
+
+document.getElementById("mt").addEventListener("input", function () {
     const inputValue = parseFloat(this.value);
-    const diff = inputValue - parseFloat(document.getElementById(`masseM`).value);
-  
+    const diff = inputValue - parseFloat(document.getElementById(`massM`).value);
+
     document.getElementById(`mf`).value = diff.toFixed(2);
-  
-    // Changement dynamique de la tolérance - mf
+
+    // Dynamic tolerance change - mf
     updateTolerance({
-      inputValue: diff,
-      toleranceId: "toleranceMF",
-      tolMax: 1050
+        inputValue: diff,
+        toleranceId: "toleranceMF",
+        tolMax: 1050
     });
-  });
-  
-  document.addEventListener("DOMContentLoaded", function () {
-    const commentaire = document.getElementById("commentaireOp");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const comment = document.getElementById("operationComment");
     const charCount = document.getElementById("charCount");
-  
-    commentaire.addEventListener("input", function () {
-      const currentLength = commentaire.value.length;
-      charCount.textContent = `${currentLength}/255 caractères`;
+
+    comment.addEventListener("input", function () {
+        const currentLength = comment.value.length;
+        charCount.textContent = `${currentLength}/255 characters`;
     });
-  });
-  
-  document.addEventListener("DOMContentLoaded", function () {
-    const commentaire = document.getElementById("commentaireQuali");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const comment = document.getElementById("qualityComment");
     const charCount = document.getElementById("charCount2");
-  
-    commentaire.addEventListener("input", function () {
-      const currentLength = commentaire.value.length;
-      charCount.textContent = `${currentLength}/255 caractères`;
+
+    comment.addEventListener("input", function () {
+        const currentLength = comment.value.length;
+        charCount.textContent = `${currentLength}/255 characters`;
     });
-  });
+});
