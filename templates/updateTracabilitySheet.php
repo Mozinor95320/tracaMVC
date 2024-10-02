aafa13520557e42ea5bf745bae3e118bcac3cee3
Moz <43842362+Mozinor95320@users.noreply.github.com>
2024-10-02 17:57:50 +0200

    hhh

--- templates/updateTracabilitySheet.php
+++ templates/updateTracabilitySheet.php
@@ -26,7 +26,7 @@
     <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
 
       <!-- Generic Data-->
-      <div class="card-body">
+      <div class="card-body d-flex justify-content-between align-items-center">
           <div class="mb-3 ml-3">
             <h2>Informations générales</h2>
             <h3>Description de la fiche de tracabilité</h3>
