1f6c32e4267d3391ff9d39aeb24a10919ecec195
Moz <43842362+Mozinor95320@users.noreply.github.com>
2024-10-02 12:12:43 +0200

    Update homepage.php

--- templates/homepage.php
+++ templates/homepage.php
@@ -19,7 +19,7 @@ foreach ($tracabilitySheets as $tracabilitySheet) {
         <div class="card-body d-flex justify-content-between align-items-center">
             <div>
                 <h5 class="card-title fw-bold"><?= htmlspecialchars($tracabilitySheet->identifier); ?></h5>
-                <em class="card-text text-muted">date de création de la fiche : <?= $tracabilitySheet->frenchCreationDate; ?></em>
+                <em class="card-text text-muted">date de création de la fiche : <?= $tracabilitySheet->sheetCreationDate; ?></em>
             </div>
             <a class="btn btn-outline-primary" href="index.php?action=openTracabilitySheet&id=<?= urlencode($tracabilitySheet->identifier) ?>">
                 <i class="bi bi-folder2-open" style="font-size: 24px;"></i> <!--Icône de dossier ouvert-->
