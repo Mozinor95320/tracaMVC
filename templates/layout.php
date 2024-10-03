21630af11ae82d77a7766ab1bda5a9618c86b834
Moz <43842362+Mozinor95320@users.noreply.github.com>
2024-10-03 15:37:11 +0200

    ggg

--- templates/layout.php
+++ templates/layout.php
@@ -24,7 +24,7 @@
         </header>
         <nav class="navbar navbar-expand-lg fixed-top bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#"><img src="templates/img/Parker.png" alt="Bootstrap" width="30" height="24">
+                <a class="navbar-brand" href="#"><img src="templates/img/Parker.png" alt="Bootstrap" height="24">
             </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
