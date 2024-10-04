f19aea24beb7e26da2cd6c312083db61e3e453f5
Moz <43842362+Mozinor95320@users.noreply.github.com>
2024-10-04 15:46:55 +0200

    hh

--- templates/updateTracabilitySheet.php
+++ templates/updateTracabilitySheet.php
@@ -4,7 +4,7 @@
 <a href="votre-url.html" class="text-decoration-none">
     <i class="bi bi-house"></i> Accueil
 </a>
-<?php echo json_encode($tracabilitySheet->$dataGraph); ?>
+<?php print_r($tracabilitySheet->$dataGraph) ?>
 <div class="container mt-4">
   
 
