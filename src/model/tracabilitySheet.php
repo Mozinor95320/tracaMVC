07ba2c81663931f79260ee27ea620772bab46f02
Moz <43842362+Mozinor95320@users.noreply.github.com>
2024-10-04 10:46:59 +0200

    ggg

--- src/model/tracabilitySheet.php
+++ src/model/tracabilitySheet.php
@@ -173,6 +173,13 @@ class TracabilitySheetRepository
         $tracabilitySheet->qualityInspectorName = $row['qualityInspectorName'];
         $tracabilitySheet->qualityInspectorRemarks = $row['qualityInspectorRemarks'];
 
+        $statement = $this->connection->getConnection()->prepare(
+            "SELECT timeLog, dancerArmPressureSetpoint, dancerArmTensionActual, postTensionActual, preTensionSetpoint, preTensionActual, hotAirBlowerSetpoint, nozzleHeaterActual, nozzleHeaterSetpoint, tapeHeaterActual, tapeHeaterSetpoint FROM windingMachineReccord WHERE idTracabiltySheet = ? ORDER BY timeLog ASC"
+        );
+
+        $statement->execute([$identifier]);
+
+        $row = $statement->fetch(PDO::FETCH_ASSOC);
 
         return $tracabilitySheet;
     }
