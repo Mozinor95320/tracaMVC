ce6bb53226c3a0b99a5f15244de809a512c7b7fb
Moz <43842362+Mozinor95320@users.noreply.github.com>
2024-10-04 11:42:34 +0200

    test

--- src/lib/database.php
+++ src/lib/database.php
@@ -4,12 +4,12 @@ namespace Application\Lib\Database;
 
 class DatabaseConnection
 {
-    public ?\PDO $database = null;
+    public PDO $database = null;
 
-    public function getConnection(): \PDO
+    public function getConnection(): PDO
     {
         if ($this->database === null) {
-            $this->database = new \PDO('mysql:host=localhost;dbname=u230474653_tracabilty;charset=utf8', 'u230474653_parker', 'Parker92700%');
+            $this->database = new PDO('mysql:host=localhost;dbname=u230474653_tracabilty;charset=utf8', 'u230474653_parker', 'Parker92700%');
         }
 
         return $this->database;
