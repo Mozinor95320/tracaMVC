10edd589cf2abe0b15a1418a531d6fe3e8ea0cb5
Moz <43842362+Mozinor95320@users.noreply.github.com>
2024-10-06 16:54:55 +0000

    hh

--- templates/layout.php
+++ templates/layout.php
@@ -22,14 +22,11 @@
 </head>
 
 <body>
-    <nav class="navbar fixed-top bg-dark border-bottom border-body" data-bs-theme="dark">
+    <nav class="navbar navbar-expand-lg fixed-top bg-dark border-bottom border-body" data-bs-theme="dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="https://mediumaquamarine-toad-850598.hostingersite.com/index.php">
                 <img src="templates/img/logoParker.png" alt="Bootstrap" width="30" height="24">
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
